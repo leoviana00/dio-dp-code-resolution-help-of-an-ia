eed8a12ef2a5ac4415dfdaedacd9f324840abb8e
Leonardo Vian Pereira <leonardoviana00@hotmail.com>
2024-10-26 08:53:53 -0300

    feat(py): concatenar dados

---
+++ resolutions/concatDados.py
@@ -0,0 +1,9 @@
+# Receber dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenar os dados
+resultado = dado1 + " " + dado2
+
+# Exibir o resultado
+print("A string concatenada é:", resultado)
